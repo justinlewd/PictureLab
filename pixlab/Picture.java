be46a5f4b73bada81356ae8c4039c5291d5da41b
justinlewd <43970004+justinlewd@users.noreply.github.com>
2019-03-11 12:45:23 -0700

    3/11

--- pixlab/Picture.java
+++ pixlab/Picture.java
@@ -129,12 +129,41 @@ public void keepOnlyBlue()
     zeroGreen();
 }
 
-
-
-
-
-    
+  public void mirrorVerticalRightToLeft()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = 0; row < pixels.length; row++)
+    {
+      for (int col = 0; col < width / 2; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][width - 1 - col];
+        leftPixel.setColor(rightPixel.getColor());
+      }
+    } 
+  }
   
+    public void mirrorHorizontal()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel topPixel = null;
+    Pixel bottomPixel = null;
+    int width = pixels[0].length;
+    for (int row = 0; row < pixels.length; row++)
+    {
+      for (int col = 0; col < width / 2; col++)
+      {
+        topPixel = pixels[row][col];
+        bottomPixel = pixels[row][width - 1 - col];
+        topPixel.setColor(bottomPixel.getColor());
+      }
+    } 
+  }
+
+
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -258,17 +287,13 @@ public void keepOnlyBlue()
     public void negate() 
   {
    Pixel[][] pixels = this.getPixels2D();
-   for(Pixel[] row : pixels)
+   for(Pixel[] rowArray : pixels)
    {
-    for(Pixel p : row) 
+    for(Pixel pixelObj : rowArray) 
     {
-     int red = p.getRed();
-     int green = p.getGreen();
-     int blue = p.getBlue();  
-     {
-      p.setRed(155-p.getRed());
-      p.setGreen(155-p.getBlue());
-     }
+     pixelObj.setGreen(255-(pixelObj.getGreen())); 
+     pixelObj.setRed(255-(pixelObj.getRed()));  
+     pixelObj.setBlue(255-(pixelObj.getBlue()));  
     }  
   }
   }
